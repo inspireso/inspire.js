0f844aad1802892b9941170a7b148a3614aa6ca2
lanxe <lan.xinen@inspire.so>
2014-06-20 18:19:44 +0800

    bugfix：jquery1.9的兼容问题，attr替换成prop

---
+++ build.bat
@@ -0,0 +1,2 @@
+@cd %~dp0
+%NODE_HOME%\spm build --include all
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-var $ = window.jQuery || require("jquery");
+define(function(require, exports, module) {
+    var $ = window.jQuery;
 
-if (!window.$doc) {
-    window.$doc = $(document);
-}
+    if (!window.$doc) {
+        window.$doc = $(document);
+    }
 
-exports = module.exports;
+    exports = module.exports;
 
-exports.submit = require('./lib/submit');
-exports.checkbox = require('./lib/checkbox');
-exports.checkbox = require('./lib/confirm');
-exports.jump = require('./lib/jump');
-exports.link = require('./lib/link');
-exports.messenger = require('./lib/messenger');
-exports.money = require('./lib/money');
-exports.number = require('./lib/number');
-exports.pagination = require('./lib/pagination');
-exports.required = require('./lib/required');
-exports.roles = require('./lib/roles');
-exports.submit = require('./lib/submit');
-exports.trim = require('./lib/trim');
-exports.validity = require('./lib/validity');
-exports.dialog = require('./lib/dialog');
-exports.async = require('./lib/async');
+    exports.submit = require('./lib/submit');
+    exports.checkbox = require('./lib/checkbox');
+    exports.checkbox = require('./lib/confirm');
+    exports.jump = require('./lib/jump');
+    exports.link = require('./lib/link');
+    exports.messenger = require('./lib/messenger');
+    exports.money = require('./lib/money');
+    exports.number = require('./lib/number');
+    exports.pagination = require('./lib/pagination');
+    exports.required = require('./lib/required');
+    exports.roles = require('./lib/roles');
+    exports.submit = require('./lib/submit');
+    exports.trim = require('./lib/trim');
+    exports.validity = require('./lib/validity');
+    exports.dialog = require('./lib/dialog');
+    exports.async = require('./lib/async');
+});
